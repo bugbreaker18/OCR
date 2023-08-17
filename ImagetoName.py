fde6f54c2af6c150456592289500b2f55d064585
Shreyansh Shukla <bugbreaker18@gmail.com>
2023-08-17 23:20:24 +0530

    Update ImagetoName.py

--- ImagetoName.py
+++ ImagetoName.py
@@ -38,3 +38,47 @@ print(text)
 cv2.imshow("Image Input", images)
 cv2.imshow("Output In Grayscale", gray)
 cv2.waitKey(0)
+
+
+# import pytesseract
+# from PIL import Image
+
+# # Path to the Tesseract executable (change this to your Tesseract installation path)
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# def preprocess_image(image):
+#     # You can add image preprocessing steps here
+#     # For example, resizing, grayscale conversion, thresholding, noise reduction, etc.
+#     # For demonstration purposes, let's just convert the image to grayscale
+#     return image.convert('L')
+
+# def extract_name_from_text(text):
+#     words = text.split()
+#     if words:
+#         generated_name = words[0]
+#     else:
+#         generated_name = "Unknown"
+#     return generated_name
+
+# def main():
+#     # Load an image using PIL (Python Imaging Library)
+#     image_path = 'path_to_your_image.png'
+#     image = Image.open(image_path)
+
+#     # Preprocess the image
+#     preprocessed_image = preprocess_image(image)
+
+#     # Use pytesseract to extract text from the preprocessed image
+#     extracted_text = pytesseract.image_to_string(preprocessed_image, lang='eng')
+
+#     # You can process the extracted text to generate a name
+#     generated_name = extract_name_from_text(extracted_text)
+
+#     print("Generated Name:", generated_name)
+
+#     # Save the extracted text to a file
+#     with open('extracted_text.txt', 'w') as f:
+#         f.write(extracted_text)
+
+# if __name__ == "__main__":
+#     main()
