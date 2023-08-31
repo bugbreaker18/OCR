9a51b336e89e65e3b99370be7e74838e5315bd56
Shreyansh Shukla <bugbreaker18@gmail.com>
2023-08-31 22:59:41 +0530

    Update OCRonPlates.py

--- OCRonPlates.py
+++ OCRonPlates.py
@@ -1,1 +1,9 @@
 # After R.CNN and Yolo, try to read the contents of the plate
+from django.contrib import admin
+from django.urls import path
+from ocr_app.views import ocr
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ocr/', ocr, name='ocr'),
+]
