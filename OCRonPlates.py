13e63e8a5fb06d4322371f6aef2e4529237d0bdc
Shreyansh Shukla <bugbreaker18@gmail.com>
2023-08-31 23:00:15 +0530

    Update OCRonPlates.py

--- OCRonPlates.py
+++ OCRonPlates.py
@@ -7,3 +7,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ocr/', ocr, name='ocr'),
 ]
+from django.shortcuts import render
+from PIL import Image
+import pytesseract
+
+def ocr(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['image']
+        image = Image.open(uploaded_file)
+        text = pytesseract.image_to_string(image)
+        return render(request, 'ocr_app/result.html', {'text': text})
+    return render(request, 'ocr_app/index.html')
